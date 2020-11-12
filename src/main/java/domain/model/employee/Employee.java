a062145fc01bb5ad643d5f51c3b5d4d1dbfb5c34
Bethany Dunwoody <b.dunwoody@kainos.com>
2020-11-12 12:07:02 +0000

    Created rough version of Employee class

--- src/main/java/domain/model/employee/Employee.java
+++ src/main/java/domain/model/employee/Employee.java
@@ -1,4 +1,67 @@
 package domain.model.employee;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private short empNum;
+    private String niNumber;
+    private int startingSalary;
+    private int bankAcc;
+    private int department;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public short getEmpNum() {
+        return empNum;
+    }
+
+    public void setEmpNum(short empNum) {
+        this.empNum = empNum;
+    }
+
+    public String getNiNumber() {
+        return niNumber;
+    }
+
+    public void setNiNumber(String niNumber) {
+        this.niNumber = niNumber;
+    }
+
+    public int getStartingSalary() {
+        return startingSalary;
+    }
+
+    public int getBankAcc() {
+        return bankAcc;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setBankAcc(int bankAcc) {
+        this.bankAcc = bankAcc;
+    }
+
+    public void setStartingSalary(int startingSalary) {
+        this.startingSalary = startingSalary;
+    }
 }
