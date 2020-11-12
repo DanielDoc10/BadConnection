7e7af63ebddb8370cea4cfc795b7f5996a83209b
Jason McMahon <j.mcmahon@kainos.com>
2020-11-12 12:30:22 +0000

    Beginning implementing user interface.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args)
     {
-        UserInterface.start();
+        UserInterface userInterface = new UserInterface();
+        userInterface.start();
     }
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,5 +1,48 @@
+import domain.model.employee.Employee;
+
+import java.util.Scanner;
+
 public class UserInterface {
-    public static void start() {
-        // not implemented
+    private Employee selectedEmployee;
+
+    public UserInterface()
+    {
+        this.
+    }
+
+    public void start() {
+        System.out.println("Welcome to the application!");
+
+        while (!isLoggedIn())
+        {
+
+            logInUser();
+
+        }
+
+        userInterface.logInUser();
+    }
+
+    private boolean isLoggedIn() {
+        boolean isLoggedIn = this.selectedEmployee != null;
+
+        return  isLoggedIn;
+    }
+
+    private void logInUser() {
+        short empNum = this.promptForEmpNum();
+        this.selectedEmployee = this.getEmployee(empNum);
+    }
+
+    private Employee getEmployee(short empNum) {
+    }
+
+    private short promptForEmpNum() {
+        System.out.println("Please enter your Employee ID");
+        Scanner sc = new Scanner(System.in);
+        short empNum = sc.nextShort();
+        sc.close();
+
+        return empNum;
     }
 }
